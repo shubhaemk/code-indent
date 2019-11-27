b1e7fb29be12286522c5f633083e9de7148f49cb
Shubham Kamath <dev.shubhaem@gmail.com>
2019-11-27 17:08:42 +0530

    Webpack Added

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+
+let config = {
+    entry : ['./src/index.js','./index.css'],
+    output : {
+        filename : 'index.js',
+        path : path.resolve(__dirname,'docs'),
+    },
+    module : {
+        rules : [
+            {
+                test : /\.js$/,
+                exclude : /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test : /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            minimize: true
+                        },
+                    },
+                    'css-loader',
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: { 
+                            minimize : true 
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins : [
+        new MiniCssExtractPlugin({
+            filename: 'index.css',
+            chunkFilename: './docs/index.css',
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: './index.html'
+        }),
+    ],
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    mode : 'production',
+};
+
+module.exports = config;
